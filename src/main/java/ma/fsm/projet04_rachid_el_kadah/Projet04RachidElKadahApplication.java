04ec370955dce5dde9306fd706c0cecbce781c06
R-ELKADAH <rachidelkadah@email.com>
2024-04-17 16:43:31 +0100

    Initial commit

---
+++ src/main/java/ma/fsm/projet04_rachid_el_kadah/Projet04RachidElKadahApplication.java
@@ -0,0 +1,13 @@
+package ma.fsm.projet04_rachid_el_kadah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet04RachidElKadahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet04RachidElKadahApplication.class, args);
+	}
+
+}
